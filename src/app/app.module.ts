e426b3ef7884c42fd91a93d478ceacee29258d58
agente6060 <isc.ma3.unix@hotmail.com>
2018-06-19 19:16:34 -0500

    Actualizacion de codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+
+const routes: Routes=[
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'addprovee', component: AddproveeComponent },
+  { path: '**', component:InicioComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
