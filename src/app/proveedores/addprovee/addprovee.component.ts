8f0c3d200d7e2629801925d65fdad175f6df746e
agente6060 <isc.ma3.unix@hotmail.com>
2018-06-21 11:48:52 -0500

    SUbir

--- src/app/proveedores/addprovee/addprovee.component.ts
+++ src/app/proveedores/addprovee/addprovee.component.ts
@@ -11,6 +11,14 @@ export class AddproveeComponent implements OnInit {
 
   proveedor : any;
 
+  provincias : string[] = [ 'Álava','Albacete','Alicante','Almería','Asturias','Ávila','Badajoz',
+                            'Barcelona','Burgos', 'Cáceres', 'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba',
+                            'La Coruña','Cuenca','Gerona','Granada','Guadalajara', 'Guipúzcoa','Huelva','Huesca',
+                            'IslasBaleares','Jaén','León','Lérida','Lugo','Madrid', 'Málaga','Murcia','Navarra','Orense',
+                            'Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria',
+                            'Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya',
+                            'Zamora','Zaragoza' ]
+
   constructor() { 
     this.proveedor = {
       nombre:'',
@@ -40,7 +48,7 @@ export class AddproveeComponent implements OnInit {
     this.proveedor.contacto = this.formpro.value.contacto;
 
     this.formpro.reset();//vaciar formulario
-    
+
   }
 
 }
